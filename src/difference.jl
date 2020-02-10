6e3999d90bccbb30a900af0a4b6daebcf245fcd3
laschuet <laschuet@users.noreply.github.com>
2020-02-10 09:51:39 +0100

    Broaden type for set differences

--- src/difference.jl
+++ src/difference.jl
@@ -107,7 +107,7 @@ function _diff(A::AbstractMatrix, B::AbstractMatrix, ia::OrderedDict,
 end
 
 """
-    diff(a::Set, b::Set)
+    diff(a::AbstractSet, b::AbstractSet)
 
 Compute the difference between set `a` and set `b`, and return a tuple
 containing the unique elements that have been shared, added, and removed.
@@ -118,4 +118,5 @@ julia> diff(Set([1, 2, 3, 3]), Set([4, 2, 1]))
 (Set([1, 2]), Set([4]), Set([3]))
 ```
 """
-diff(a::Set, b::Set) = intersect(a, b), setdiff(b, a), setdiff(a, b)
+diff(a::AbstractSet, b::AbstractSet) =
+    intersect(a, b), setdiff(b, a), setdiff(a, b)
